14a3e1eb9f1960d59357ce571c232c4b76dafc70
YaelCas <146982278+YaelCas@users.noreply.github.com>
2023-10-04 11:38:50 -0600

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.ndimage.interpolation import zoom
 from streamlit_drawable_canvas import st_canvas
 from utils import process_image
-st.markdown("# Digit :blue[Recognition] :green[App] :pencil: 💻🤖")
+st.markdown("# :Purple[Aplicacion de reconocimiento de numeros <3]     ¯\_(ツ)_/¯      (☞ﾟヮﾟ)☞")
 
 # Load trained model
 model = tf.keras.models.load_model('mi_modelo.h5')
